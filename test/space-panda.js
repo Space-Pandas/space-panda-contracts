f16f39ef700f31a2bde9d32a11df260a15dfde91
picolabs <picolabs@163.com>
2021-08-04 11:34:16 +0800

    integrate with hardhat

--- test/space-panda.js
+++ test/space-panda.js
@@ -1,25 +1,13 @@
-const SpacePanda = artifacts.require("SpacePanda");
+const { assert, expect } = require("chai");
 
-contract("space panda test", async accounts => {
-    it("should deploy successfully", async () => {
-        const panda = await SpacePanda.deployed();
-        const name = await panda.name();
-        const symbol = await panda.symbol();
-        assert.equal(name, "SpacePanda");
-        assert.equal(symbol, "SP");
-    });
-
-    it("should deploy successfully", async () => {
-        const panda = await SpacePanda.deployed();
-    });
-
-    /**it("should mint successfully", async () => {
-        const panda = await SpacePanda.deployed();
-        for(let i=0; i<200; i++) {
-            await panda.airDropNFT(accounts[1])
-        }
-        await panda.airDropNFT(accounts[1])
-        const balance = await panda.balanceOf(accounts[1]);
-        console.log(accounts[1], balance.toString())
-    });**/
+describe("space panda test", async () => {
+  it("should deploy successfully", async () => {
+    const Panda = await ethers.getContractFactory("SpacePanda");
+    const panda = await Panda.deploy("SpacePanda", "SP");
+    await panda.deployed();
+    const name = await panda.name();
+    const symbol = await panda.symbol();
+    assert.equal(name, "SpacePanda");
+    assert.equal(symbol, "SP");
+  });
 });
