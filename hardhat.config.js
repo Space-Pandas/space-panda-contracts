b5db357120fa51317b1ff784a701bbd12fb7c69e
pandadadig <pandadadig@gmail.com>
2021-08-19 22:02:26 +0800

    add networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,11 +27,23 @@ task("balance", "Prints an account's balance")
 module.exports = {
   solidity: "0.7.6",
     networks: {
-        hardhat: {
-            accounts: {
-                accountsBalance: "50000000000000000000000"
-            }
-        },
+      hardhat: {
+          accounts: {
+              accountsBalance: "50000000000000000000000"
+          }
+      },
+      bsc_testnet: {
+        url: "https://data-seed-prebsc-2-s3.binance.org:8545/",
+        accounts: [
+          "31d65c4c48af8c69b6956da054c3c3a73cb2c030188dbed23df8b477d234f652",
+        ],
+      },
+      bsc: {
+        url: "https://binance.ankr.com",
+        accounts: [
+          "to be stored somewhere else",
+        ],
+      },
     },
   mocha: {
     timeout: 60 * 3600 * 1000
